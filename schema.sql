87fffe89035cc0006c7c38611095d73bfffd2d58
放羊娃 <ices99@sina.com>
2017-03-06 13:34:40 +0800

    修改sql
    插入语句

--- schema.sql
+++ schema.sql
@@ -7,7 +7,9 @@ CREATE TABLE IF NOT EXISTS `todolist` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-insert into todolist(id, user_id, title, status, create_time) values(1, 1, '习近平五谈稳中求进织密扎牢民生保障网', '0', 1482214350), (2, 1, '特朗普获超270张选举人票将入主白 宫', '1', 1482214350);
+insert into todolist(id, user_id, title, status, create_time) values(1, 1, '习近平五谈稳中求进织密扎牢民生保障网', '0', 1482214350)
+ ,(2, 1, '特朗普获超270张选举人票将入主白 宫', '1', 1482214350)
+ ,(2, 1, '测试数据萨兰达；撒娇打死打伤了；的', '1', 1482214350);
 
 
 CREATE TABLE `user` (
